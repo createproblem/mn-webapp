ac05c88a7e0091c98b75d99196c64de3b96ac65d
createproblem <createproblem@gmx.de>
2013-05-15 21:17:52 +0200

    changes for unit testing with travis ci

--- src/g5/HomeBundle/Tests/Controller/DefaultControllerTest.php
+++ src/g5/HomeBundle/Tests/Controller/DefaultControllerTest.php
@@ -1,4 +1,5 @@
 <?php
+// /src/g5/HomeBundle/Tests/Controller/DefaultControllerTest.php
 
 namespace g5\HomeBundle\Tests\Controller;
 
@@ -10,8 +11,8 @@ class DefaultControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+        $crawler = $client->request('GET', '/');
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("Node.js")')->count() > 0);
     }
 }
