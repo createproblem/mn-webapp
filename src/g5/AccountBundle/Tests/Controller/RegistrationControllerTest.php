1715ab3883421ab63afe7311ac4a21fd83c00824
createproblem <createproblem@gmx.de>
2013-06-18 15:21:13 +0200

    more tests

--- src/g5/AccountBundle/Tests/Controller/RegistrationControllerTest.php
+++ src/g5/AccountBundle/Tests/Controller/RegistrationControllerTest.php
@@ -2,31 +2,13 @@
 
 namespace g5\AccountBundle\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use g5\AccountBundle\Tests\AccountAwareWebTestCase;
 
-class RegistrationControllerTest extends WebTestCase
+class RegistrationControllerTest extends AccountAwareWebTestCase
 {
     public function testRegister()
     {
-        $time = time();
-        $client = static::createClient();
-        $client->followRedirects();
-
-        $crawler = $client->request('GET', '/account/register/');
-
-        $buttonCrawlerNode = $crawler->selectButton('Register');
-
-        $form = $buttonCrawlerNode->form();
-
-        $form['fos_user_registration_form[username]'] = $time;
-        $form['fos_user_registration_form[email]'] = $time.'@example.org';
-        $form['fos_user_registration_form[plainPassword][first]'] = $time;
-        $form['fos_user_registration_form[plainPassword][second]'] = $time;
-        // $form['fos_user_registration_form[termsOfService]']->tick();
-
-        $crawler = $client->submit($form);
-
-        $this->assertTrue($crawler->filter('html:contains("The user has been created successfully")')->count() > 0);
+        $this->addUser();
     }
 }
 
--- src/g5/MovieBundle/Tests/Controller/MovieControllerTest.php
+++ src/g5/MovieBundle/Tests/Controller/MovieControllerTest.php
@@ -3,74 +3,71 @@
 
 namespace g5\MovieBundle\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use g5\AccountBundle\Tests\AccountAwareWebTestCase;
 
-use Symfony\Component\BrowserKit\Cookie;
-use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
-
-class MovieControllerTest extends WebTestCase
+class MovieControllerTest extends AccountAwareWebTestCase
 {
-    private $client = null;
+    public function testAdd()
+    {
+        $client = static::createClient();
+        $this->login($client);
 
-    // public function testAdd()
-    // {
-    //     $this->login();
+        $crawler = $client->request('GET', '/movie/add/550');
+        $this->assertTrue($client->getResponse()->isSuccessful());
 
-    //     $crawler = $this->client->request('GET', '/movie/add/550');
-    //     $this->assertTrue($this->client->getResponse()->isSuccessful());
+        $content = $client->getResponse()->getContent();
+        $content = json_decode($content);
 
-    //     $content = $this->client->getResponse()->getContent();
-    //     $content = json_decode($content);
-
-    //     $this->assertEquals(550, $content);
-    // }
+        $this->assertEquals(550, $content);
+    }
 
     public function testSearch()
     {
-        $this->login();
+        $client = static::createClient();
+        $this->login($client);
 
-        $crawler = $this->client->request('GET', '/movie/search');
+        $crawler = $client->request('GET', '/movie/search');
 
-        $this->assertTrue($this->client->getResponse()->isSuccessful());
+        $this->assertTrue($client->getResponse()->isSuccessful());
         $this->assertEquals(1, $crawler->filter("button[id=btnSearch]")->count());
 
         $form = $crawler->selectButton('btnSearch')->form();
 
         $form['g5_movie_search[search]'] = "";
-        $crawler = $this->client->submit($form);
-        $this->assertTrue($this->client->getResponse()->isNotFound());
+        $crawler = $client->submit($form);
+        $this->assertTrue($client->getResponse()->isNotFound());
 
         $form['g5_movie_search[search]'] = "Fight Club";
-        $crawler = $this->client->submit($form);
+        $crawler = $client->submit($form);
         $this->assertGreaterThan(0, $crawler->filter('html:contains("Fight Club")')->count());
     }
 
-    public function testLoadmeta()
-    {
-        $this->login();
+    // public function testLoadmeta()
+    // {
+    //     $this->login();
 
-        $crawler = $this->client->request('POST', '/movie/loadmeta/550');
+    //     $crawler = $this->client->request('POST', '/movie/loadmeta/550');
 
-        $this->assertTrue($this->client->getResponse()->isSuccessful());
+    //     $this->assertTrue($this->client->getResponse()->isSuccessful());
 
-        $content = $this->client->getResponse()->getContent();
-        $content = json_decode($content);
+    //     $content = $this->client->getResponse()->getContent();
+    //     $content = json_decode($content);
 
-        $this->assertTrue($content instanceof \stdClass);
-    }
+    //     $this->assertTrue($content instanceof \stdClass);
+    // }
 
-    private function login()
-    {
-        $this->client = static::createClient();
-        $session = $this->client->getContainer()->get('session');
+    // private function login()
+    // {
+    //     $this->client = static::createClient();
+    //     $session = $this->client->getContainer()->get('session');
 
-        $firewall = 'main';
-        $token = new UsernamePasswordToken('admin', null, $firewall, array('ROLE_ADMIN'));
+    //     $firewall = 'main';
+    //     $token = new UsernamePasswordToken('admin', null, $firewall, array('ROLE_ADMIN'));
 
-        $session->set('_security_'.$firewall, serialize($token));
-        $session->save();
+    //     $session->set('_security_'.$firewall, serialize($token));
+    //     $session->save();
 
-        $cookie = new Cookie($session->getName(), $session->getId());
-        $this->client->getCookieJar()->set($cookie);
-    }
+    //     $cookie = new Cookie($session->getName(), $session->getId());
+    //     $this->client->getCookieJar()->set($cookie);
+    // }
 }
