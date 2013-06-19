90c6b409371644d39f8da269f67ce931f219b3dd
createproblem <createproblem@gmx.de>
2013-06-19 16:44:44 +0200

    removie useless stuff added more and better tests
    Signed-off-by: createproblem <createproblem@gmx.de>

--- src/g5/AccountBundle/Tests/Controller/RegistrationControllerTest.php
+++ src/g5/AccountBundle/Tests/Controller/RegistrationControllerTest.php
@@ -1,14 +1,42 @@
 <?php
 
+/*
+* This file is part of the mn-webapp package.
+*
+* (c) gogol-medien <https://github.com/gogol-medien/>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
 namespace g5\AccountBundle\Tests\Controller;
 
-use g5\AccountBundle\Tests\AccountAwareWebTestCase;
+require_once dirname(__DIR__).'/../../../../app/g5WebTestCase.php';
 
-class RegistrationControllerTest extends AccountAwareWebTestCase
+class RegistrationControllerTest extends \g5WebTestCase
 {
     public function testRegister()
     {
-        $this->addUser();
+        $client = static::createClient();
+        $client->followRedirects();
+
+        $crawler = $client->request('GET', '/account/register/');
+
+        $buttonCrawlerNode = $crawler->selectButton('Register');
+
+        $form = $buttonCrawlerNode->form();
+
+        $form['fos_user_registration_form[username]'] = 'test';
+        $form['fos_user_registration_form[email]'] = 'test'.'@example.org';
+        $form['fos_user_registration_form[plainPassword][first]'] = 'test';
+        $form['fos_user_registration_form[plainPassword][second]'] = 'test';
+        // $form['fos_user_registration_form[termsOfService]']->tick();
+
+        $crawler = $client->submit($form);
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertTrue($crawler->filter('html:contains("The user has been created successfully")')->count() > 0);
+
+        $this->deleteUser($client, 'test');
     }
 }
 
