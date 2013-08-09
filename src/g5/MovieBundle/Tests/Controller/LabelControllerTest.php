4c1451c9ab4142e8b1b21bbf03499fc64914938b
createproblem <createproblem@gmx.de>
2013-08-09 05:25:12 +0200

    bufix in tests fixed

--- src/g5/MovieBundle/Tests/Controller/LabelControllerTest.php
+++ src/g5/MovieBundle/Tests/Controller/LabelControllerTest.php
@@ -17,6 +17,7 @@ class LabelControllerTest extends \g5WebTestCase
 {
     private $client;
     private $mm;
+    private $lm;
 
     public function setUp()
     {
@@ -24,6 +25,7 @@ class LabelControllerTest extends \g5WebTestCase
 
         $this->client = static::createClient();
         $this->mm = $this->client->getContainer()->get('g5_movie.movie_manager');
+        $this->lm = $this->client->getContainer()->get('g5_movie.label_manager');
     }
 
     public function testIndexAction()
@@ -35,12 +37,23 @@ class LabelControllerTest extends \g5WebTestCase
         $movie->addLabel($label);
         $this->mm->updateMovie($movie);
 
+        $tmdbMock = $this->getTmdbMock();
+        $tmdbMock->expects($this->any())
+            ->method('getImageUrl')
+            ->with('w185')
+            ->will($this->returnValue(''))
+        ;
+
+        static::$kernel->setKernelModifier(function($kernel) use ($tmdbMock) {
+            $kernel->getContainer()->set('g5_tools.tmdb.api', $tmdbMock);
+        });
+
         $crawler = $this->client->request('GET', '/movie/label/'.$label->getNameNorm());
-print($this->client->getResponse()->getContent());
+
         $this->assertTrue($this->client->getResponse()->isSuccessful());
         $this->assertEquals(1, $crawler->filter('h4:contains("'.$movie->getTitle().'")')->count());
 
-        $this->deleteLabel($label);
-        $this->deleteMovie($movie);
+        $this->mm->removeMovie($movie);
+        $this->lm->removeLabel($label);
     }
 }
