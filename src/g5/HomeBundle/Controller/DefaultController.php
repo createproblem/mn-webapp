77e784eaea8d1298c98cde41aacbcb998d0f39a1
createproblem <createproblem@gmx.de>
2014-04-24 00:03:10 +0200

    added latest movies to index

--- src/g5/HomeBundle/Controller/DefaultController.php
+++ src/g5/HomeBundle/Controller/DefaultController.php
@@ -13,24 +13,17 @@ class DefaultController extends Controller
         }
 
         $mm = $this->get('g5_movie.movie_manager');
-        $lm = $this->get('g5_movie.label_manager');
         $user = $this->getUser();
         $tmdbApi = $this->get('g5_tools.tmdb.api');
 
-        $text = "Hello World"; //10
-        $truncated = (strlen($text) > 5) ? substr($text, 0, 20) . '...' : $text;
+        // $text = "Hello World"; //10
+        // $truncated = (strlen($text) > 5) ? substr($text, 0, 20) . '...' : $text;
 
-        $labels = $lm->findLabelsBy(array('user' => $user), array('movie_count' => 'DESC'));
         $latestMovies = $mm->loadLatestMovies($user);
-        $randomMovies = $mm->loadRandomMovies($user, 25);
-        shuffle($randomMovies);
 
         return $this->render('g5HomeBundle:Default:test.html.twig', array(
             'latestMovies' => $latestMovies,
-            'labels' => $labels,
-            'randomMovies' => $randomMovies,
             'imgUrl_w1280' => $tmdbApi->getImageUrl('w1280'),
-            'imgUrl_w92' => $tmdbApi->getImageUrl('w92'),
         ));
     }
 }
